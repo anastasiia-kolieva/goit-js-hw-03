e9b08d1612c74c43cdeba0460e866fc08842e077
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-15 23:18:01 +0300

    started to make task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true,
+};
+
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
+
+// сформировала массив userInformation из ключей обьекта user
+const userInformation = Object.keys(user);
+
+// выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of  ????
