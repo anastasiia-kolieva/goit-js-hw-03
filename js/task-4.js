13d475d04d17c534265e0a81ec175170808276cc
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-18 18:55:44 +0300

    made task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,33 @@
 "use strict";
+
+const countTotalSalary = function (employees) {
+  let totalSalary = [];
+  let total = 0;
+
+  totalSalary = Object.values(employees);
+
+  for (let i = 0; i < totalSalary.length; i += 1) {
+    total += totalSalary[i];
+  }
+
+  console.log(total);
+
+  return;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+countTotalSalary({}); // 0
+
+countTotalSalary({
+  mango: 100,
+  poly: 150,
+  alfred: 80,
+}); // 330
+
+countTotalSalary({
+  kiwi: 200,
+  lux: 50,
+  chelsy: 150,
+}); // 400
